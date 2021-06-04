3265425d70b51d26f549326a0a58bf57d458367b
YUKI "Piro" Hiroshi <piro.outsider.reflex@gmail.com>
2021-06-04 14:20:18 +0900

    Fix indent

--- Configs.js
+++ Configs.js
@@ -103,13 +103,13 @@ class Configs {
             // storage.managed.get() fails on options page in Thunderbird.
             // The problem should be fixed by Thunderbird side.
             if (window.messenger) {
-                setTimeout(() => {
-                  if (resolved)
-                    return;
-                  resolved = true;
-                  this._log('load: failed to load managed storage: timeout');
-                  resolve(null);
-                }, 250);
+              setTimeout(() => {
+                if (resolved)
+                  return;
+                resolved = true;
+                this._log('load: failed to load managed storage: timeout');
+                resolve(null);
+              }, 250);
             }
           });
         })(),
